e64a37e515be7eebbb5113c842de0e2b12faabb3
freddyaboulton <alfonsoboulton@gmail.com>
2021-10-22 15:08:58 -0400

    Add include dir

--- recipe/build_script.sh
+++ recipe/build_script.sh
@@ -1,1 +1,2 @@
-$PYTHON -m pip install . -vv --global-option --cmake-options="-DSTATIC_LINK_VW_JAVA=On;-DPython3_EXECUTABLE=\"$PYTHON\""
+INCLUDE=$(python -c "from sysconfig import get_paths;print(get_paths()['include'])")
+$PYTHON -m pip install . -vv --global-option --cmake-options="-DSTATIC_LINK_VW_JAVA=On;-DPython3_EXECUTABLE=\"$PYTHON\";-DPython_INCLUDE_DIR=\"$INCLUDE\""
