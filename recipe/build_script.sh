c4febc66d1b7973ca5d64c63d1ec1988a962a8e6
freddyaboulton <alfonsoboulton@gmail.com>
2021-10-22 15:26:53 -0400

    Show include

--- recipe/build_script.sh
+++ recipe/build_script.sh
@@ -1,3 +1,4 @@
 INCLUDE=$($PYTHON -c "from sysconfig import get_paths;print(get_paths()['include'])")
 echo $INCLUDE
+ls $INCLUDE
 $PYTHON -m pip install . -vv --global-option --cmake-options="-DSTATIC_LINK_VW_JAVA=On;-DPython3_EXECUTABLE=\"$PYTHON\";-DPython_INCLUDE_DIR=\"$INCLUDE\""
