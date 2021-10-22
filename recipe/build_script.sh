0247dab021d8b40d8829359e538bce52562a4054
freddyaboulton <alfonsoboulton@gmail.com>
2021-10-22 15:19:52 -0400

    Use

--- recipe/build_script.sh
+++ recipe/build_script.sh
@@ -1,2 +1,3 @@
-INCLUDE=$(python -c "from sysconfig import get_paths;print(get_paths()['include'])")
+INCLUDE=$($PYTHON -c "from sysconfig import get_paths;print(get_paths()['include'])")
+echo $INCLUDE
 $PYTHON -m pip install . -vv --global-option --cmake-options="-DSTATIC_LINK_VW_JAVA=On;-DPython3_EXECUTABLE=\"$PYTHON\";-DPython_INCLUDE_DIR=\"$INCLUDE\""
