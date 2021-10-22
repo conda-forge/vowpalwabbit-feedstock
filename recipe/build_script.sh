82dee0959583e61f8730443abddf9e8257021310
freddyaboulton <alfonsoboulton@gmail.com>
2021-10-22 15:46:00 -0400

    Find boost

--- recipe/build_script.sh
+++ recipe/build_script.sh
@@ -1,4 +1,5 @@
 INCLUDE=$($PYTHON -c "from sysconfig import get_paths;print(get_paths()['include'])")
 echo $INCLUDE
 ls $INCLUDE
+locate boost
 $PYTHON -m pip install . -vv --global-option --cmake-options="-DSTATIC_LINK_VW_JAVA=On;-DPython3_EXECUTABLE=\"$PYTHON\";-DPython_INCLUDE_DIR=\"$INCLUDE\""
